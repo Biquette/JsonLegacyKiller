c7a02fa82238d9e8a3fc1b28ae5fecee61d4b79e
Chloé MAHALIN <chloe.mahalin@itametis.com>
2016-10-30 14:13:06 +0100

    add licences

--- src/main/java/com/itametis/jsonconverter/classpathscan/JsonMappingPackage.java
+++ src/main/java/com/itametis/jsonconverter/classpathscan/JsonMappingPackage.java
@@ -1,3 +1,22 @@
+/* This file is part of JsonLegacyKiller.
+ *
+ * JsonLegacyKiller is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JsonLegacyKiller is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with JsonLegacyKiller.  If not, see <http://www.gnu.org/licenses/gpl.txt >.
+ *
+ * If you need to develop a closed-source software, please contact us
+ * at 'social@itametis.com' to get a commercial version of JsonLegacyKiller,
+ * with a proprietary license instead.
+ */
 package com.itametis.jsonconverter.classpathscan;
 
 import com.google.common.collect.ImmutableSet;
